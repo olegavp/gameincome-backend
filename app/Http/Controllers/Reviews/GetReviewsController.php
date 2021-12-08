2d17fd11686b196685283ab5645bc66a5374874d
Evster <8961240@gmail.com>
2021-12-08 19:40:44 +0700

    поправил auth и получение reviews

--- app/Http/Controllers/Reviews/GetReviewsController.php
+++ app/Http/Controllers/Reviews/GetReviewsController.php
@@ -15,47 +15,35 @@ class GetReviewsController extends Controller
 {
     public function get(): JsonResponse|AnonymousResourceCollection
     {
-        try
-        {
+        try {
             $reviews = Review::query()->with('writer', 'comments', 'views')->orderByDesc('created_at')->paginate(5);
 
-            $reviewsGames = array();
-            $reviewsSoftware = array();
-            foreach ($reviews as $review)
-            {
-                if ($review->item_type === 'game')
-                {
+            $reviewsGames = [];
+            $reviewsSoftware = [];
+            foreach ($reviews as $review) {
+                if ($review->item_type === 'game') {
                     array_push($reviewsGames, $review->item_id);
-                }
-                elseif ($review->item_type === 'software')
-                {
+                } elseif ($review->item_type === 'software') {
                     array_push($reviewsSoftware, $review->item_id);
                 }
             }
 
-            if (count($reviewsGames) > 0)
-            {
+            if (count($reviewsGames) > 0) {
                 $games = Game::query()->whereIn('id', $reviewsGames)->get();
-                foreach ($reviews as $review)
-                {
-                    foreach ($games as $game)
-                    {
-                        if ($review->item_id === $game->id)
-                        {
+                foreach ($reviews as $review) {
+                    foreach ($games as $game) {
+                        if ($review->item_id === $game->id) {
                             $review['item'] = $game;
                         }
                     }
                 }
             }
-            elseif (count($reviewsSoftware) > 0)
-            {
+
+            if (count($reviewsSoftware) > 0) {
                 $software = Software::query()->whereIn('id', $reviewsSoftware)->get();
-                foreach ($reviews as $review)
-                {
-                    foreach ($software as $oneSoftware)
-                    {
-                        if ($review->item_id === $oneSoftware->id)
-                        {
+                foreach ($reviews as $review) {
+                    foreach ($software as $oneSoftware) {
+                        if ($review->item_id === $oneSoftware->id) {
                             $review['item'] = $oneSoftware;
                         }
                     }
@@ -64,11 +52,11 @@ class GetReviewsController extends Controller
 
             return ReviewsResource::collection($reviews)
                 ->additional(['status' => 200]);
-        }
-        catch (\Throwable)
-        {
-            return response()->json(['error' => 'Произошла ошибка при загрузке всех обзоров. Попробуйте перезагрузить страницу, если ошибка остаётся, то обратитесь в поддержку, Спасибо!',
-                'status' => 400], 400);
+        } catch (\Throwable) {
+            return response()->json([
+                'error' => 'Произошла ошибка при загрузке всех обзоров. Попробуйте перезагрузить страницу, если ошибка остаётся, то обратитесь в поддержку, Спасибо!',
+                'status' => 400,
+            ], 400);
         }
     }
 }
